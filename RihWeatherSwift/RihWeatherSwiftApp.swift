6322787335b9a1dc97c4900f3271603ff6e12b75
Naimul Kabir <kabirnayeem.99@gmail.com>
2024-10-05 21:07:05 +0600

    Initial Commit

---
+++ RihWeatherSwift/RihWeatherSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RihWeatherSwiftApp.swift
+//  RihWeatherSwift
+//
+//  Created by Naimul Kabir on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RihWeatherSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
